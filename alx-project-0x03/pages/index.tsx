710fc511acc68abcbf547d7df998e2e78578ac72
BissyClassic <rachaelafolabi2021@gmail.com>
2025-09-27 19:36:52 +0100

    Add updated index.tsx files

--- alx-project-0x03/pages/index.tsx
+++ alx-project-0x03/pages/index.tsx
@@ -2,7 +2,7 @@ import Button from "@/components/common/Button";
 import { useRouter } from "next/router";
 
 interface PageRouteProps {
-  pageRoute: string
+  pageRoute: string;
 }
 
 export default function Home() {
